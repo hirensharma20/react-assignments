d204f0186800611cda175da7989d6ad28ec23c90
hirensharma20 <143261420+hirensharma20@users.noreply.github.com>
2024-03-31 13:51:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Template from './Template_task/Template'
+import State_task from './Task/State_task'
+import List_task from './Task/List_task'
+import One from './Task/One'
+
+function App() {
+  return (
+    <div>
+    <Template/>
+    {/* <State_task/> */}
+    {/* <List_task/> */}
+    {/* <One/> */}
+    </div>
+  )
+}
+
+export default App
