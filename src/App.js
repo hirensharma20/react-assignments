3636389e22d5f47aacf9c36dc53feb19ad96d583
hirensharma20 <143261420+hirensharma20@users.noreply.github.com>
2024-05-13 12:46:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import Template from './Template_task/Template'
-import State_task from './Task/State_task'
-import List_task from './Task/List_task'
-import One from './Task/One'
 
 function App() {
   return (
     <div>
     <Template/>
-    {/* <State_task/> */}
-    {/* <List_task/> */}
-    {/* <One/> */}
     </div>
   )
 }
